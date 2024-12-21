c1debdf8293eaeb0dd70563e69dca2418781f60a
Lavie <lavie43b@gmail.com>
2024-12-21 21:46:14 +0000

    Update map tile server URLs based on theme

--- components/map/map.tsx
+++ components/map/map.tsx
@@ -16,7 +16,9 @@ const center: LatLngExpression = [32.089456319281176, 34.893238387602985];
 export default function MyMap(props: any) {
     const {resolvedTheme} = useTheme();
 
-    const SERVER_MAP = `${process.env.NEXT_PUBLIC_MAP_IP}/styles/${resolvedTheme === "light" ? "positron" : "dark"}/{z}/{x}/{y}.png`;
+	const SERVER_MAP = resolvedTheme === "light" 
+		? "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+		: "https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png";
     
     return (
 		<>

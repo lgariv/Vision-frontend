1b7b74cb97d4f7841afdb6c9e8f2d676b9e26afd
Lavie <lavie43b@gmail.com>
2025-01-06 14:56:09 +0000

    Remove Open Graph meta tags from layout component and replaced with a different method

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import MainNavbar from "@/components/main-navbar/navigation-bar";
 import { Pixelify_Sans, Heebo } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import Head from "next/head";
 
 const heebo = Heebo({
 	subsets: ["latin"],
@@ -40,12 +39,6 @@ export default function RootLayout({
 		>
 			<Analytics />
 			<SpeedInsights />
-			<Head>
-				<meta property="og:image" content="/preview.png" />
-				<meta property="og:image:type" content="image/png" />
-				<meta property="og:image:width" content="1901" />
-				<meta property="og:image:height" content="1026" />
-			</Head>
 			<body className={inter.className}>
 				<ThemeProvider
 					attribute="class"
